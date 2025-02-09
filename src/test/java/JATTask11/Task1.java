bc0dce8c80a47772401623b694bc28d0fa1a319f
Palaniraj17 <Palanibala@LAPTOP-BD96GJ6G>
2025-02-09 10:25:24 +0530

    JATTask 11 answers

--- src/test/java/JATTask11/Task1.java
+++ src/test/java/JATTask11/Task1.java
@@ -1,9 +1,42 @@
 package JATTask11;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+	public static void main(String[] args) throws InterruptedException {
+		String expectText = "New Window";
 
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/windows");
+
+		String mainWindow = driver.getWindowHandle();
+		driver.findElement(By.linkText("Click Here")).click();
+
+		Set<String> windows = driver.getWindowHandles();
+		List<String> allwindows = new ArrayList<String>(windows);
+		int totalwindow = allwindows.size();
+		for (int i = 0; i <= totalwindow; i++) {
+			if (driver.getPageSource().contains(expectText)) {
+
+				System.out.println("User currently on new window page and New Window text is displaying");
+				String newWindowID = driver.getWindowHandle();
+				if (!newWindowID.equals(mainWindow)) {
+					driver.close();
+					driver.switchTo().window(mainWindow);
+					System.out.println("Now the user is on main page");
+				}
+			} else {
+				driver.switchTo().window(allwindows.get(i));
+			}
+		}
+		driver.quit();//Giving this to avoid connection reset error
+	}
 }
--- src/test/java/JATTask11/Task2.java
+++ src/test/java/JATTask11/Task2.java
@@ -1,9 +1,49 @@
 package JATTask11;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task2 {
 
-	public static void main(String[] args) {
-		System.out.println("Java");
-	}
+	public static void main(String[] args) throws InterruptedException {
 
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://the-internet.herokuapp.com/nested_frames");
+
+		driver.switchTo().frame("frame-top");
+		List<WebElement> frames = driver.findElements(By.tagName("frame"));
+		if (frames.size() == 3) {
+			System.out.println("The total frames in top section is 3");
+		}
+
+		driver.switchTo().frame("frame-left");
+		if (driver.getPageSource().contains("LEFT")) {
+			System.out.println("LEFT text is available in top-left frame");
+		}
+
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-middle");
+		if (driver.getPageSource().contains("MIDDLE")) {
+			System.out.println("MIDDLE text is available in top-middle frame");
+		}
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-right");
+		if (driver.getPageSource().contains("RIGHT")) {
+			System.out.println("RIGHT text is available in top-right frame");
+		}
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame("frame-bottom");
+		if (driver.getPageSource().contains("BOTTOM")) {
+			System.out.println("BOTTOM text is available in bottom frame");
+		}
+		// The web page is not having any title currently , hence we cannot verify the title
+		driver.quit();//Giving this to avoid connection reset error
+	}
 }
